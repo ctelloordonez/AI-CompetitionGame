c21180b802830f2a475795b3b6ed667455d8c9c7
ctelloordonez <ctelloordonez@users.noreply.github.com>
2019-03-05 12:43:47 +0100

    Shooting Functionality Added

---
+++ AI-CompetitionGame/Assets/Scripts/Shell.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public float damage = 100f;
+    public float lifeTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Tank target = other.GetComponent<Tank>();
+
+        if (target != null)
+        {
+            target.TakeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ AI-CompetitionGame/Assets/Scripts/TankShooting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShooting : MonoBehaviour
+{
+    public Rigidbody shell;
+    public Transform fireTransform;
+    public float speed;
+    public float cooldown;
+
+    private bool fired;
+    private float timeShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeShot = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && timeShot <= 0)
+        {
+            Fire();
+        }
+
+        if (timeShot > 0)
+        {
+            timeShot -= Time.deltaTime;
+        }
+    }
+
+    private void Fire()
+    {
+        fired = true;
+        Rigidbody shellInstance = Instantiate(shell, fireTransform.position, fireTransform.rotation) as Rigidbody;
+        shellInstance.velocity = speed * fireTransform.forward;
+        timeShot = cooldown;
+    }
+}
