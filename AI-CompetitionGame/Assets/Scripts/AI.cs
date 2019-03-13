a6173b4b3269e27c3bdf7adaa15c9a730dc5252c
Moobesoor <edorb721@hotmail.com>
2019-03-13 16:06:28 +0100

    ai navigation kinda

---
+++ AI-CompetitionGame/Assets/Scripts/AI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour{
+
+    public float LookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    #region Singleton
+    public static AI instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+#endregion
+
+    public GameObject Enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = AI.instance.Enemy.transform;
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= LookRadius)
+        {
+            agent.SetDestination(target.position); 
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, LookRadius);
+    }
+}
