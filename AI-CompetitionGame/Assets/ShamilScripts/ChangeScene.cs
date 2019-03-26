f39cea36eb8350a77071f10b69712a491b9e130e
shamil1233 <43807064+shamil1233@users.noreply.github.com>
2019-03-26 21:45:16 +0100

    menu

--- AI-CompetitionGame/Assets/ShamilScripts/ChangeScene.cs
+++ AI-CompetitionGame/Assets/ShamilScripts/ChangeScene.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-
 public class ChangeScene : MonoBehaviour
-{ 
+{
 
+
+    // Start is called before the first frame update
     public void Start()
     {
        
